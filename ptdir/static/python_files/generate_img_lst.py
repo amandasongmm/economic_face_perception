bb6b351a24a7460e2b172926f4e552646e8e7cbe
amandasongmm <feijuejuanling@gmail.com>
2019-01-18 01:02:20 -0800

    gt-attractive local pc to git

--- ptdir/static/python_files/generate_img_lst.py
+++ ptdir/static/python_files/generate_img_lst.py
@@ -1,8 +1,13 @@
 import os
 import glob
+import shutil
 from shutil import copyfile
 import pandas as pd
 import numpy as np
+from pylab import imread
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+
 
 
 def gen_img_lst(key_word):
@@ -36,7 +41,6 @@ def gen_img_lst(key_word):
                 if i in file_name:
                     df[i].iloc[0] += 1
                     category_dict[i].append(file_name)
-
 
         # Now, randomly pick up 8+2 elements from each category.
         fixed_lst = []  # 2*8=64
@@ -89,8 +93,121 @@ def gen_img_lst(key_word):
 
         # Then manually copy the txt into the chicago_lst.js file.
 
+    if key_word == 'gt-100-attractive':
+        dst_dir = '../images/attractive/'
+        img_dir = '/static/images/attractive/'
+        all_lst = glob.glob(dst_dir+'*.jpg')
 
-gen_img_lst('mit')
+        with open('gt-100-attractive.txt', 'w') as f:
+            f.write("var all_lst = [\n")
+            for cur_face_path in all_lst:
+                file_name = os.path.basename(cur_face_path)
+                f.write("{imgname: '" + img_dir + file_name + "'},\n")
+            f.write('];\n')
+
+        print('Done!')
+
+
+def gen_img_collage():
+    img_dir = '../images/attractive/'
+    all_im_lst = glob.glob(img_dir + '*.jpg')
+
+    columns = 6
+    rows = 5
+
+    for im_id in range(0, 3):
+        print im_id
+
+        fig = plt.figure()
+
+        for i in range(1, columns * rows + 1):
+
+            cur_im_path = all_im_lst[im_id * columns * rows + i -1]
+            image = imread(cur_im_path)
+
+            fig.add_subplot(rows, columns, i)
+            plt.axis('off')
+            plt.imshow(image)
+
+        fig.savefig('../images/collage/'+str(im_id)+'.png')
+
+
+def gen_gt_stim_lst():
+
+    trait_lst = ['attractive', 'aggressive', 'trustworthy', 'intelligent']
+
+    rating_df = pd.read_csv('/Users/amanda/Dropbox/2019/modifAE/celeb_ratings.csv')
+
+    n_unique_num = 80
+    n_rep_num = 20
+
+    img_src_dir = '/Users/amanda/Dropbox/dataset/celebA/oneface/'
+
+    for trait in trait_lst:
+
+        print trait
+
+        dst_dir = '../images/celeba_gt_crop/'+trait+'/'
+
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+
+        cur_df = rating_df[['Filename', trait]]
+        cur_min, cur_max = cur_df[trait].min(), cur_df[trait].max()
+
+        anchor_lst = np.linspace(cur_min, cur_max, num=n_unique_num)
+        cur_names, cur_values = [], []
+
+        for i in range(n_unique_num):
+
+            anchor_value = anchor_lst[i]
+            cur_df['distance'] = (cur_df[trait]-anchor_value).abs()
+            cur_df = cur_df.sort_values(by=['distance'])
+            cur_im_name = cur_df.iloc[0]['Filename']
+            shutil.copy(img_src_dir+cur_im_name, dst_dir)
+
+            cur_names.append(cur_im_name)
+            cur_values.append(cur_df.iloc[0][trait])
+            cur_df.drop(cur_df.index[0], inplace=True)  # remove the selected faces so it won't appear twice.
+
+        stim_df = pd.DataFrame(columns=[['Filename', trait, 'repeat']])
+        stim_df['Filename'] = cur_names
+        stim_df[trait] = cur_values
+        stim_df['repeat'] = 0
+        print stim_df['Filename'].nunique()
+
+        stim_repeat = stim_df.sample(n_rep_num)
+        stim_repeat['repeat'] = 1
+
+        print stim_repeat['Filename'].nunique()
+
+        stim_df = pd.concat([stim_df, stim_repeat], ignore_index=True)
+        stim_df = shuffle(stim_df)
+        stim_df = stim_df.sample(frac=1, random_state=1)
+        print stim_df['Filename'].nunique()
+
+        save_csv_name = '../../../preparation_data/amt_gt_validation/'+trait+'_stim_lst'+'.csv'
+        # save_txt_name = '../../../preparation_data/amt_gt_validation/'+trait+'.txt'
+        stim_df.to_csv(save_csv_name)
+
+    for trait in trait_lst:
+        print trait
+        dst_dir = '/static/images/celeba_gt_crop/' + trait + '/'
+        save_csv_name = '../../../preparation_data/amt_gt_validation/'+trait+'_stim_lst'+'.csv'
+        save_txt_name = '../../../preparation_data/amt_gt_validation/'+trait+'.txt'
+        stim_df = pd.read_csv(save_csv_name)
+
+        with open(save_txt_name, 'w') as f:
+            f.write("var all_lst = [\n")
+            for file_name in stim_df['Filename'].values:
+                f.write("{imgname: '" + dst_dir + file_name + "'},\n")
+            f.write('];\n')
+
+
+if __name__ == '__main__':
+    gen_gt_stim_lst()
+
+
 
 
 
